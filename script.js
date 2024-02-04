fcde63d8b856ae220ac4e4688a202fcb741634ba
adityax4 <advensicaddy@gmail.com>
2024-02-04 16:48:01 +0530

    Add change grid button

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 const container = document.querySelector(".container");
+const btn = document.querySelector(".makeGrid");
 
-for (let i = 1; i <= 4; i++) {
-    const row = document.createElement("div");
-    row.classList.add("row");
-    container.appendChild(row);
-    for (let j = 1; j <= 4; j++) {
-        const item = document.createElement("div");
-        item.classList.add("item");
-        row.appendChild(item);
+let gridSize=10;
+
+btn.addEventListener('click', ()=>{
+    gridSize= prompt("Enter grid size: ");
+    createGrid(parseInt(gridSize, 10));
+})
+
+function createGrid(size) {
+    // Clear existing grid
+    container.innerHTML = "";
+
+    for (let i = 1; i <= size; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        container.appendChild(row);
+        for (let j = 1; j <= size; j++) {
+            const item = document.createElement("div");
+            item.classList.add("item");
+            row.appendChild(item);
+        }
     }
 }
+
+createGrid(gridSize);
